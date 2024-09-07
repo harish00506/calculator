bd2bb2abe9058065586cf727e6c933657a9a78de
harish00506 <harish00506@outlook.com>
2024-09-07 07:06:02 +0530

    part 2

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 console.log("hello World!");
 // const btn7 = document.getElementById("value-7");
+var val;
+const op = document.getElementById("input-ele");
 
 
 document.querySelectorAll(".btn-style").forEach(button =>{
     button.addEventListener('click', () =>{
-        var val = button.value;
+        val = button.value;
+        console.log(val);
+        if(val !== "C" && val !== "="){
+            op.innerText = val;
+        }
+        if(val === "C"){
+            input1.value = 0;
+            input2.value = 0;
+        }
+        if(val === "="){
+            let input1Val = input1.value;
+            let input2Val = input2.value;
+            let result;
+            const ans = document.getElementById("ans");
+            if(op === "+"){
+                result = input1Val + input2Val;
+                ans.innerText =  result;
+            }
+        }
     });
 });
 
 const input1 = document.getElementById("input-1");
 const input2 = document.getElementById("input-2");
 
-console.log(input1.value + val + input2.value);
 
